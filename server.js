ce9e93cc648717d9b434fdf8192d4d6d5bbff9c3
vanshWGTS <vanshdeep@wishgeekstechserve.com>
2026-06-26 23:10:43 +0530

    fix cors

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.post("/api/mail",async(req,res)=>{
     try{
     const {email,phone,fullName,websiteURL,industry,message}=req?.body;
  if(!email || !phone || !fullName || !industry)return res.status(400).json({status:false,message:"Fields are missing"})
-    await sendEnquiryMail({email,phone,websiteURL,industry,message});
+    await sendEnquiryMail({email,fullName,phone,websiteURL,industry,message});
 return res.status(200).json({status:true,message:"Mail send successfull"})
     }catch(err){
         return res.status(500).json({status:false,message:"Internal server error",error:err})
